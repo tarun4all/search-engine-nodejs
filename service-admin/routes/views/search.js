a22e267c26f4fe811bb402b3677d3f1023508f38
Ujjwal Rastogi <rastogiujj12@gmail.com>
2020-02-05 13:24:40 +0530

    merged

---
+++ service-admin/routes/views/search.js
@@ -0,0 +1,15 @@
+module.exports = async (req, res) => {
+    if(req.query.search) res.send(await getSearchResult(req.query.search, '', req.query.page||1));
+    else res.redirect('/');
+};
+
+async function getSearchResult(keyword, engine, page){
+    // console.log('hi');
+    // console.log('ser',services.searchService);
+    let data = await services.searchService.search(keyword,engine,page).catch((err) => {console.log("error occures")});
+    // let data = [];
+    // let adv = await services.advCampaignService.getAdvCampaign('test').catch((err)=> {console.log(err);});
+    // if(adv) data.push(adv);
+    console.log('>>>',data);
+    return data;
+}
