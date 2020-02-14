0c8617fdfbe6842224fb05f365f24a046f6f3338
Ujjwal Rastogi <rastogiujj12@gmail.com>
2020-02-14 15:29:36 +0530

    made a constatnt for number of search results in a page and tested with bing API key

--- service-admin/services/searchService.js
+++ service-admin/services/searchService.js
@@ -1,6 +1,7 @@
 const SerpWow = require('google-search-results-serpwow');
 const serpwow = new SerpWow(process.env.SURP_API_KEY || 'E5E8DCD07B864C8CBA7F728B0F53F388');
 const fetch = require('node-fetch');
+const SEARCH_COUNT = 9;
 
 
 exports = module.exports = class SearchService {
@@ -8,9 +9,17 @@ exports = module.exports = class SearchService {
         let data = {};
         let res = await this.serpwowSearch(keyword, engine, page); //not working fine
         console.log('serpwow res', res);
-        // console.log('/n/n/n/n/n');
         if(res){
-            data.organic_results = res.organic_results;
+            data.organic_results = [];
+            res.organic_results.forEach((el)=>{
+                let temp = {};
+                temp.position  = el.position;
+                temp.title = el.title;
+                temp.link = el.link;
+                temp.snippet = el.snippet;
+                data.organic_results.push(temp);
+            });
+
             data.social = {
                 tweets : [],
             };
@@ -23,19 +32,22 @@ exports = module.exports = class SearchService {
         else {
             console.log('in else');
             res = await this.getResponseFromBingAPI(keyword, page); //working fine
-            console.log('bing res', res);
+            // console.log('bing res', res.webPages.value);
             if(!res) return('some error has occured');
             else {
-                data.organic_results = res.webPages.value;
-                data.organic_results.forEach((el)=>{
-                    el.title = el.name;
-                    el.link = el.url;
-                    el.position = el.id;
+                data.organic_results = [];
+                res.webPages.value.forEach((el)=>{
+                    let temp = {};
+                    temp.title = el.name;
+                    temp.link = el.url;
+                    temp.position = el.id;
+                    temp.snippet = el.snippet;
+                    data.organic_results.push(temp);
                 })
             }
         }
         console.log('data',res);
-        data.currPage = page;
+        // data.currPage = page;
         return data;
     }
 
@@ -48,12 +60,14 @@ exports = module.exports = class SearchService {
                     q: keyword,
                     page: page || 1,
                     engine: engine,
-                    country_code: 'US'
+                    country_code: 'US',
+                    num: SEARCH_COUNT,
                 }
             } else {
                 params = {
                     q: keyword,
                     page: page || 1,
+                    num: SEARCH_COUNT,
                     gl: 'us',
                     hl: 'en',
                     location: 'United States',
@@ -67,19 +81,17 @@ exports = module.exports = class SearchService {
                 })
                 .catch(error => {
                     console.log('serpwow catch');
-                    return reject();
+                    return resolve();
 
                 });
         });
-     }
+    }
 
     async getResponseFromBingAPI(keyword,page= 1) {
-        // console.log('inside bing');
-
-        const COUNT = 9;
-        const OFFSET = (page-1)*COUNT;
+        // console.log('keyword ', keyword, ' page', page);
+        const OFFSET = (page-1)*SEARCH_COUNT;
         try {
-            let url = process.env.BING_ENDPOINT + '?q=' + keyword + '&customconfig='+ process.env.BING_CUSTOM_CONFIG_ID + "&count=" + COUNT + "&offset=" + OFFSET + '&mkt=en-US';
+            let url = process.env.BING_ENDPOINT + '?q=' + keyword  + "&count=" + SEARCH_COUNT + "&offset=" + OFFSET + '&mkt=en-US';
             const response = await fetch(url, {headers:{
                     'Ocp-Apim-Subscription-Key': process.env.BING_API_KEY,
                 }});
@@ -87,6 +99,7 @@ exports = module.exports = class SearchService {
         } catch (error) {
             console.log(error);
         }
+
     };
 
 };
