6b6b6433c315328dc131bf8bd77c485359dd0e16
Ujjwal Rastogi <rastogiujj12@gmail.com>
2020-03-20 12:22:05 +0530

    homescreen was not looking good in smaller devices, fixed that

--- service-web/src/app/screens/homeScreen.js
+++ service-web/src/app/screens/homeScreen.js
@@ -5,13 +5,12 @@ export class HomeScreen extends Component {
     render() {
         //TODO fix searchInput component to be able to search from here
         return (
-            <div className="wrapper-section">
+            <div className="wrapper-section result-wrapper">
             <div className="google-search-row">
                 <div className="iner-section">
                     <div className="search-logo">
                         <img src="/images/googlelogo.png" alt="logo"/>
                     </div>
-                    {/*<ProjectSection/>*/}
                     <SearchInput query = {{q:""}}/>
                     <p className="search-des">The search engine that respects your privacy.</p>
                 </div>
